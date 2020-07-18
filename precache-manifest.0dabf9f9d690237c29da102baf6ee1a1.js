86556242f510a8ff538cb63a9ab0c8f2ff258e3b
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2020-07-19 03:14:49 +0800

    Updates

---
+++ precache-manifest.0dabf9f9d690237c29da102baf6ee1a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1093a4ca2b244ba0031ae8d9b1ad589b",
+    "url": "/Figma-Assignment/index.html"
+  },
+  {
+    "revision": "c25b6bf9a828f776d665",
+    "url": "/Figma-Assignment/static/css/main.ab115176.chunk.css"
+  },
+  {
+    "revision": "2a7e045859da5513ad98",
+    "url": "/Figma-Assignment/static/js/2.9dee396d.chunk.js"
+  },
+  {
+    "revision": "acff744c5c5e21e2b14a11e29451820a",
+    "url": "/Figma-Assignment/static/js/2.9dee396d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25b6bf9a828f776d665",
+    "url": "/Figma-Assignment/static/js/main.e8d6c480.chunk.js"
+  },
+  {
+    "revision": "2106683f2fb25765858c",
+    "url": "/Figma-Assignment/static/js/runtime-main.24f986d3.js"
+  }
+]);
